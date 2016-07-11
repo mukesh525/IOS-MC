479c3879a18f8ac21cc1b97f9b3593882e75acb4
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-07-11 10:27:35 +0000

    Filter Module Added

--- trunk/MCube/MCube/MenuController.swift
+++ trunk/MCube/MCube/MenuController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+
+
+
+
+
 class MenuController: UITableViewController {
 
     @IBOutlet var FilterTable: UITableView!
@@ -51,88 +56,22 @@ class MenuController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
   override  func viewDidAppear(animated: Bool){
-//    do {
-//        let myPath = NSIndexPath(forRow: 1, inSection: 0)
-//        if(self.mytableview != nil){
-//            mytableview.selectRowAtIndexPath(myPath, animated: false, scrollPosition: UITableViewScrollPosition.None)}
-//        } catch _ {
-//      
-//    }
+    if let select = NSUserDefaults.standardUserDefaults().objectForKey("select"){
+       let myPath = NSIndexPath(forRow: select as! Int, inSection: 0)
+        mytableview.selectRowAtIndexPath(myPath, animated: false, scrollPosition: UITableViewScrollPosition.None)
+    }
     
     }
     
-    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
-
-//    
-//    // MARK: - Table view data source
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // Return the number of sections.
-//        return 1
-//    }
-//    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // Return the number of rows in the section.
-//        return options.count
-//    }
-//
-//    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("filter", forIndexPath: indexPath) as! FilterCell
-//        let data: OptionsData = self.options[indexPath.row] as! OptionsData
-//        cell.layer.cornerRadius=15
-//        cell.layer.borderColor = UIColor.orangeColor().CGColor
-//        cell.layer.borderWidth = 2
-//        cell.backgroundColor = UIColor.clearColor()
-//        cell.filter.text=data.value
-//        
-//       return cell
-//    }
-// 
-//
-//  
-//    // Override to support conditional editing of the table view.
-//    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-//        // Return NO if you do not want the specified item to be editable.
-//        return true
-//    }
-//  
-//
-//   
-//    // Override to support editing the table view.
-//    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-//        if editingStyle == .Delete {
-//            // Delete the row from the data source
-//            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-//        } else if editingStyle == .Insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }    
-//    }
-//    
-//    // Override to support rearranging the table view.
-//    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-//
-//    }
-//    
-//
-// 
-//    
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
 //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        if(segue.identifier == "sw_right")
-//        {
-//            let someText = "Text"
+//        if(segue.identifier == "sw_rear"){
 //            let rvc:FollowUpViewController = segue.destinationViewController as! FollowUpViewController
-//            rvc.programVar = someText
-//        }    }
-//   
+//           
+//        }
+//    }
+   
+   
 
 }
