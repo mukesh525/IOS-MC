425eb7f795885ae793f9d40b5da43a5c3f4ef5b9
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-07-12 12:40:11 +0000

    remember me module

---
+++ MCube/CheckBox.swift
@@ -0,0 +1,38 @@
+//
+//  CheckBox.swift
+//  MCube
+//
+//  Created by Mukesh Jha on 12/07/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "checked")! as UIImage
+    let uncheckedImage = UIImage(named: "check")! as UIImage
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                
+                self.setImage(checkedImage, forState: .Normal)
+            } else {
+                self.setImage(uncheckedImage, forState: .Normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.isChecked = false
+    }
+    
+    func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
