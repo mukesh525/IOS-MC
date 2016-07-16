731bc3223762022aedfec2f3f3bcb50ff8382a9d
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-07-16 18:52:40 +0000

    customeview phase one

--- trunk/MCube/MCube/CustomeCell4.swift
+++ trunk/MCube/MCube/CustomeCell4.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class CustomeCell4: UITableViewCell {
+class CustomeCell4: UITableViewCell ,UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var chekboxTable: UITableView!
     @IBOutlet weak var label1: UILabel!
+    var optionsList = [OptionsData]()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        chekboxTable.delegate = self
+        chekboxTable.dataSource = self
+        chekboxTable.allowsSelection=false
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -22,5 +27,42 @@ class CustomeCell4: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    //MARK: - Tableview Delegate & Datasource
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        return optionsList.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        let cell1 = tableView.dequeueReusableCellWithIdentifier("checkcell", forIndexPath: indexPath) as!ChekboxCell
+        let optdata: OptionsData = self.optionsList[indexPath.row]
+        cell1.checklabel.text=optdata.value
+        cell1.Chekbox.on = optdata.isChecked
+        return cell1
+            
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+               return 44
+    }
+    
+    
+    
+    
+    
+
 
 }
