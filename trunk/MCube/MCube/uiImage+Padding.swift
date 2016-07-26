28472f12cd7666b943d66093988eec78531a1c15
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-07-26 07:12:24 +0000

    utils extension update

--- trunk/MCube/MCube/uiImage+Padding.swift
+++ trunk/MCube/MCube/uiImage+Padding.swift
@@ -14,3 +14,59 @@ extension UIImage {
   }
 }
 
+extension UIDatePicker {
+    func getStringValue() -> String {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let formatteddate = dateFormatter.stringFromDate(self.date)
+        return formatteddate
+    }
+}
+
+extension String {
+
+func getDateFromString() -> String {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    dateFormatter.timeZone = NSTimeZone(name: "UTC")
+    guard let date = dateFormatter.dateFromString(self) else {
+        assert(false, "no date from string")
+        return ""
+    }
+    dateFormatter.dateFormat = "dd-MM-yyyy"
+    let timeStamp = dateFormatter.stringFromDate(date)
+    return timeStamp
+   }
+    
+    func getTimeFromString() -> String {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone(name: "UTC")
+        guard let date = dateFormatter.dateFromString(self) else {
+            assert(false, "no date from string")
+            return ""
+        }
+        dateFormatter.dateFormat = "hh:mm a"
+        let timeStamp = dateFormatter.stringFromDate(date)
+        return timeStamp
+    }
+    
+    func convertDateFormater() -> NSDate {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone(name: "UTC")
+        
+        guard let date = dateFormatter.dateFromString(self) else {
+            assert(false, "no date from string")
+            return NSDate()
+        }
+        //let timeStamp = dateFormatter.stringFromDate(date)
+        return date
+    }
+    
+    
+    
+    
+}
+
+
