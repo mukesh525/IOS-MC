e8bb95c60baf8a18cfe60a3d3d34de421f61dfcf
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-08-23 04:57:08 +0000

    login design raw change

---
+++ MCube/CustomPointAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  CustomPointAnnotation.swift
+//  MCube
+//
+//  Created by Mukesh Jha on 18/08/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CustomPointAnnotation: MKPointAnnotation {
+    var imageName: String!
+}
