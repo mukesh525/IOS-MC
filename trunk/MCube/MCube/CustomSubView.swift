5500fb57e3cf3532030329662755923823609fd5
mukesh <mukesh@d9fe211c-c3a4-45e4-9331-6c46727caf4a>
2016-09-03 10:14:03 +0000

    map view show location model static

---
+++ trunk/MCube/MCube/CustomSubView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomSubView.swift
+//  MCube
+//
+//  Created by Mukesh Jha on 03/09/16.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomSubView: UIView {
+
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var name: UILabel!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
